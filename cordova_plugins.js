00934396971c48cdbadbc1ecd395a330ebfa1ef0
1011 <hola@1011.mx>
2013-08-26 13:47:08 -0500

    Carga inicial

---
+++ cordova_plugins.js
@@ -0,0 +1,96 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.network-information/www/network.js",
+        "id": "org.apache.cordova.core.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.network-information/www/Connection.js",
+        "id": "org.apache.cordova.core.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.core.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.core.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/Position.js",
+        "id": "org.apache.cordova.core.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.core.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.core.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.core.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.AudioHandler/www/MediaError.js",
+        "id": "org.apache.cordova.core.AudioHandler.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.AudioHandler/www/Media.js",
+        "id": "org.apache.cordova.core.AudioHandler.Media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.dialogs/www/notification.js",
+        "id": "org.apache.cordova.core.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.core.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.core.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+]
+});
